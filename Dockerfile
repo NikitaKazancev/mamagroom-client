3d8a0c642ab82573bdf7e04f3450989eb69358bf
Nike <nkaz2003@gmail.com>
2024-08-03 15:14:21 +0300

    fix: Changing color
    Changing color to blue and brown

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:current-slim
+
+WORKDIR /client
+
+COPY /src /client/src
+COPY /public /client/public
+# COPY .env /client/
+COPY next-env.d.ts /client/
+COPY next.config.mjs /client/
+COPY postcss.config.mjs /client/
+COPY tailwind.config.ts /client/
+COPY tsconfig.json /client/
+# COPY bun.lockb /client/
+COPY package.json /client/
+COPY package-lock.json /client/
+
+# RUN npm install -g bun
+# RUN bun install
+RUN npm install
+RUN npm run build
+
+EXPOSE 80
+
+CMD ["npm", "run", "start"]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Footer } from '@/modules/footer/footer'
 import { Header } from '@/modules/header/header'
-import { MainAboutUsSection } from '@/pages/main/about-us-section/about-us-section'
-import { MainMainSection } from '@/pages/main/main-section/main-section'
-import { MainProcedures } from '@/pages/main/procedures/procedures'
-import { MainReviewsSection } from '@/pages/main/reviews-section/reviews-section'
+import { MainAboutUsSection } from '@/page/main/about-us-section/about-us-section'
+import { MainMainSection } from '@/page/main/main-section/main-section'
+import { MainProcedures } from '@/page/main/procedures/procedures'
+import { MainReviewsSection } from '@/page/main/reviews-section/reviews-section'
 
 export default function Home() {
 	return (
