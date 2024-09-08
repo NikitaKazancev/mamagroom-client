35c0669097948867a409bea3d2ebda2c1b2898d9
NikitaKazancev <nkaz2003@gmail.com>
2024-09-08 15:47:13 +0300

    public env domain

--- src/utils/cookies-client.ts
+++ src/utils/cookies-client.ts
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie'
 
 export const saveAccessToken = async (accessToken: string) => {
 	Cookies.set('accessToken', accessToken, {
-		domain: 'localhost',
+		domain: process.env.NEXT_PUBLIC_DOMAIN,
 		sameSite: 'strict',
 		expires: 1,
 	})
